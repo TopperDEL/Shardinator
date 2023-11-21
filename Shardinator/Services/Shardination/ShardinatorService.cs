b423bf4a80563ca92b016d56649c13b3e686ca3f
TopperDEL <github_com@tparth.de>
2023-11-21 14:40:53 +0100

    feat: Shardinate thumbnails, too

--- Shardinator/Services/Shardination/ShardinatorService.cs
+++ Shardinator/Services/Shardination/ShardinatorService.cs
@@ -26,22 +26,18 @@ public class ShardinatorService : IShardinatorService
         {
             string targetPath = media.CreationDate.Year + "/" + media.CreationDate.Month.ToString("D2") + "/" + media.CreationDate.Day.ToString("D2") + "/" + media.Name;
 
-            using (uplink.NET.Models.Access access = new uplink.NET.Models.Access(_localSecretsStore.GetSecret(StorjAuthenticationService.ACCESS_GRANT)))
+            var thumbnailShardinated = await ShardinateAsync("thumb/" + targetPath, media.ThumbnailStream);
+            if(thumbnailShardinated)
             {
-                var bucketService = new BucketService(access);
-                var bucket = await bucketService.GetBucketAsync(_localSecretsStore.GetSecret(StorjAuthenticationService.BUCKET)).ConfigureAwait(false);
-                var objectService = new ObjectService(access);
-                var upload = await objectService.UploadObjectAsync(bucket, targetPath, new uplink.NET.Models.UploadOptions(), media.MediaStream, false).ConfigureAwait(false);
-                upload.UploadOperationProgressChanged += Upload_UploadOperationProgressChanged;
-                if (!_token.IsCancellationRequested)
+                var fileShardinated = await ShardinateAsync(targetPath, media.MediaStream);
+                if(!fileShardinated)
                 {
-                    await upload.StartUploadAsync().ConfigureAwait(false);
-
-                    if (!upload.Completed)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
+            }
+            else
+            {
+                return false;
             }
 
             if (!_token.IsCancellationRequested)
@@ -55,6 +51,29 @@ public class ShardinatorService : IShardinatorService
         {
             return false;
         }
+    }
+
+    private async Task<bool> ShardinateAsync(string targetPath, Stream fileData)
+    {
+        using (uplink.NET.Models.Access access = new uplink.NET.Models.Access(_localSecretsStore.GetSecret(StorjAuthenticationService.ACCESS_GRANT)))
+        {
+            var bucketService = new BucketService(access);
+            var bucket = await bucketService.GetBucketAsync(_localSecretsStore.GetSecret(StorjAuthenticationService.BUCKET)).ConfigureAwait(false);
+            var objectService = new ObjectService(access);
+            var upload = await objectService.UploadObjectAsync(bucket, targetPath, new uplink.NET.Models.UploadOptions(), fileData, false).ConfigureAwait(false);
+            upload.UploadOperationProgressChanged += Upload_UploadOperationProgressChanged;
+            if (!_token.IsCancellationRequested)
+            {
+                await upload.StartUploadAsync().ConfigureAwait(false);
+
+                if (!upload.Completed)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     private void Upload_UploadOperationProgressChanged(uplink.NET.Models.UploadOperation uploadOperation)
